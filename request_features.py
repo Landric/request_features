7a44e2a5f2ea2aec243de6ff17b5bb36c24f4a9e
Tom Blount <tb12g09@ecs.soton.ac.uk>
2017-08-02 12:58:06 +0100

    Initial commit

---
+++ request_features.py
@@ -0,0 +1,30 @@
+
+def analyse(filename):
+
+    extract_keywords()
+    extract_locations()
+    extract_dates()
+    extract_fileformats()
+    extract_licences()
+
+def extract_keywords():
+    pass
+
+
+def extract_locations():
+    pass
+
+
+def extract_dates():
+    pass
+
+
+def extract_fileformats():
+    pass
+
+
+def extract_licences():
+    pass
+
+if __name__ == "__main__":
+    analyse("requests.txt")
